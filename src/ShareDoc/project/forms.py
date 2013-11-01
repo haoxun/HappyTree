0223e7c7893fde0a142f7354bf79698bb6e3118c
Haoxun Zhan <peter@haoxun.local>
2013-11-02 00:20:16 +0800

    adjust code  based on pep8

--- src/ShareDoc/project/forms.py
+++ src/ShareDoc/project/forms.py
@@ -1,31 +1,33 @@
 from __future__ import unicode_literals
-
 from django import forms
-from real_group.forms import GroupNameHandlerForm, AddUserForm
+from real_group.forms import GroupNameHandlerForm
+from real_group.forms import AddUserForm
 from project.models import Project
 from real_group.models import RealGroup
 from guardian.models import User
 
+
 class ProjectNameHandlerForm(forms.Form):
-    
-    name = forms.CharField(required=True, 
-                           max_length=50, 
+    name = forms.CharField(required=True,
+                           max_length=50,
                            min_length=3)
+
     def clean(self):
         if 'project_name_submit' not in self.data \
                 and 'create_project_submit' not in self.data:
             raise forms.ValidationError('Not Being Submitted')
         return self.cleaned_data
 
+
 class ProjectDescriptionHandlerForm(forms.Form):
     description = forms.CharField(required=False,
                                   max_length=500)
+
     def clean(self):
         if 'project_description_submit' not in self.data \
                 and 'create_project_submit' not in self.data:
             raise forms.ValidationError('Not Being Submitted')
         return self.cleaned_data
-
 
 
 class AddRealGroupForm(GroupNameHandlerForm):
@@ -34,16 +36,18 @@ class AddRealGroupForm(GroupNameHandlerForm):
             return self._add_real_group_set
         else:
             return "None"
+
     add_real_group_set = property(_get_add_real_group_set)
 
     def clean(self):
         if 'PTR_submit' not in self.data:
             raise forms.ValidationError('Not Being Submitted')
         name = self.cleaned_data.get('name', None)
-        if name == None:
+        if name is None:
             return self.cleaned_data
         self._add_real_group_set = RealGroup.objects.filter(name__icontains=name)
         return self.cleaned_data
+
 
 class AddUserForm(AddUserForm):
     def clean(self):
@@ -62,6 +66,7 @@ class ApplyToProjectForm(ProjectNameHandlerForm):
             return self._add_project_set
         else:
             return "None"
+
     add_project_set = property(_get_add_project_set)
 
     def clean(self):
@@ -73,6 +78,7 @@ class ApplyToProjectForm(ProjectNameHandlerForm):
         self._add_project_set = Project.objects.filter(name__icontains=name)
         return self.cleaned_data
 
+
 class RealGroupApplyToProjectForm(ApplyToProjectForm):
     def clean(self):
         if 'RTP_submit' not in self.data:
@@ -82,7 +88,3 @@ class RealGroupApplyToProjectForm(ApplyToProjectForm):
         name = self.cleaned_data['name']
         self._add_project_set = Project.objects.filter(name__icontains=name)
         return self.cleaned_data
-
-
-
-
