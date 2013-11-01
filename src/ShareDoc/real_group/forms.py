a75b7d0d7a4277bb137375bb21077a8232d87fb4
Haoxun Zhan <peter@haoxun.local>
2013-11-01 23:36:41 +0800

    adjust code  based on pep8

--- src/ShareDoc/real_group/forms.py
+++ src/ShareDoc/real_group/forms.py
@@ -1,12 +1,15 @@
 from __future__ import unicode_literals
 from django import forms
-from guardian.models import Group, User
+from guardian.models import User
+from guardian.models import Group
 from real_group.models import RealGroup
 
+
 class GroupNameHandlerForm(forms.Form):
-    name = forms.CharField(required=True, 
-                           max_length=50, 
+    name = forms.CharField(required=True,
+                           max_length=50,
                            min_length=3)
+
     def clean(self):
         # following pattern is reservered for group structure of project.
         if 'group_name_submit' not in self.data \
@@ -14,7 +17,8 @@ class GroupNameHandlerForm(forms.Form):
             raise forms.ValidationError("Not Being Submit")
 
         return self.cleaned_data
-        
+
+
 class GroupDescriptionHandlerForm(forms.Form):
     description = forms.CharField(required=False,
                                   max_length=5000)
@@ -47,6 +51,7 @@ class AddUserForm(forms.Form):
                                                  id__gte=0)
         return self.cleaned_data
 
+
 class ApplyToGroupForm(GroupNameHandlerForm):
     def _get_add_group_set(self):
         if hasattr(self, '_add_group_set'):
@@ -62,12 +67,10 @@ class ApplyToGroupForm(GroupNameHandlerForm):
         # see _clean_fields
         # https://github.com/django/django/blob/master/django/forms/fields.py
         # see clean
-        # if a field valiate some rules, that value would not be in the cleaned_data
+        # if a field valiate some rules,
+        # that value would not be in the cleaned_data
         if 'name' not in self.cleaned_data:
             return self.cleaned_data
         name = self.cleaned_data['name']
         self._add_group_set = RealGroup.objects.filter(name__icontains=name)
         return self.cleaned_data
-
-
-
