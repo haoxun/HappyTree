897fd37d6f73dbbff9b6e90b2a2bcab67b074426
Haoxun Zhan <peter@haoxun.local>
2013-11-06 20:10:18 +0800

    apply style to group list page

--- src/ShareDoc/real_group/static/real_group/group_list.js
+++ src/ShareDoc/real_group/static/real_group/group_list.js
@@ -1,10 +1,45 @@
+var clear_search_result = function(hide_dom, show_dom) {
+	show_dom.find('div.search_result').html("");
+}
+
 $(function() {
-	hide_dom('#apply_to_project > div, #creat_project > div');
-	set_trigger_link("#apply_to_project a, #creat_project a");
-	set_cancel_button("#apply_to_project :button, #creat_project :button");
+	$('div.apply_confirm, div.create_group').hide();
+	set_trigger_link(
+		'#apply_to_group a',
+		'#apply_to_group',
+		'div.apply_confirm',
+		null,
+		clear_search_result
+	);
+	set_cancel_button(
+		'#apply_to_group :button',
+		'#apply_to_group',
+		'div.apply_confirm',
+		null
+	);
 });
 
 $(function() {
-	set_search_form('#apply_to_project form');
-	set_create_form('#creat_project form');
+	set_create_form('div.create_group form');
+	set_trigger_link(
+		'#create_group_div a',
+		'#create_group_div',
+		'div.create_group',
+		null	
+	);
+	set_cancel_button(
+		'#create_group_div :button',
+		'#create_group_div',
+		'div.create_group',
+		null	
+	);
+});
+
+$(function() {
+	set_search_form(
+		'div.apply_confirm form',
+		'div.apply_confirm',
+		'p.error',
+		'div.search_result'
+	);
 });
