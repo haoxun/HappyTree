227c5a2df7f0e6a0ca7153542ed557f2548ac8dd
Haoxun Zhan <peter@haoxun.local>
2013-11-14 17:31:53 +0800

    fix JS bug on creation of message

--- src/ShareDoc/message/static/message/create_message.js
+++ src/ShareDoc/message/static/message/create_message.js
@@ -8,6 +8,21 @@ var set_create_message_button = function(common_ancestor, hide) {
 		var url = $(this).attr('href');
 		$.get(url);
 	});
+	common_ancestor.find('div.widget_loadder').find('div.post_message').find('form').submit(function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		var form = $(this);
+		var post_str = form.serialize();
+		var submit_name = form.find('input[type="submit"]').attr('name');
+		post_str += ('&' + submit_name + '=');
+		var url = form.attr('action');
+		$.post(url, post_str, function() {
+			load_message_list();
+			common_ancestor.find('div.widget_loadder').slideUp('fast');
+			hide.fadeIn('fast');
+		});
+		return false;		
+	});
 }
 $(function() {
 	set_widget_trigger(
