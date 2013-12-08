07e259e1201b24f1876e1806d4009f0c26ffc172
Haoxun Zhan <peter@haoxun.local>
2013-12-08 11:28:27 +0800

    update

--- src/ShareDoc/project/static/project/project_file_list.js
+++ src/ShareDoc/project/static/project/project_file_list.js
@@ -1,14 +1,33 @@
-var load_file_list = function() {
-	$('#file_list').load('.', {'load_file_list': null}, function() {
-		$('#file_list a.remove').click(function(event) {
-			event.preventDefault();
-			var url = $(this).attr('href');
-			$.get(url, load_file_list);
+var set_remove = function(file_list) {
+	file_list.find('a.remove').click(function(event) {
+		event.preventDefault();
+		var file_item = $(this).parent();
+		var url = $(this).attr('href');
+		var file_pointer_id = $(this).attr('file_pointer_id');
+		$.ajax({
+			type: 'DELETE',
+			url: url,
+		    data: {file_pointer_id: file_pointer_id}
+		}).success(function() {
+			file_item.fadeOut('fast', function() {
+				file_item.remove();
+			});
 		});
-
 	});
 }
 
+var load_file_list = function() {
+	var file_list = $('#file_list');
+	file_list.fadeOut('fast');
+	$.post('.', {'load_file_list': null}, function(data) {
+		file_list.html(data);
+		file_list.fadeIn('fast');
+		set_remove(file_list);
+	});
+}
+
+
+
 $(function() {
-	load_file_list.call();
+	load_file_list();
 });
